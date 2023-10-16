5dca9510d74fb4ea8b2312a579b9e14e55ad0338
Adrian Gesimba <gesimbaadrian@gmail.com>
2023-10-16 11:18:04 +0300

    controlled search component intro

--- frontendd/src/App.js
+++ frontendd/src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/search';
+
+
+
+
 function App() {
+  const [word, setWord]=useState("");
+
+  const handleSearchSubmit=(e)=>{
+    e.preventDefault();
+    console.log(word)
+  }
+  console.log(word);
+
   return (
     <div className="App">
       <Header />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
--- frontendd/src/components/Header.js
+++ frontendd/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Navbar} from 'react-bootstrap';
 
+const navbarStyle={
+  backgroundColor:"red"
+};
+
 const Header =() => {
     return(
-        <Navbar bg="light" data-bs-theme="light">
+        <Navbar style={navbarStyle} data-bs-theme="light">
           <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
       </Navbar>
     )

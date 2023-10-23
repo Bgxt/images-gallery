5f5b001b3d657c87e8d05d3f7aba2b657e2269bf
Adrian Gesimba <gesimbaadrian@gmail.com>
2023-10-23 12:13:37 +0300

    fixed code and eslint together with prettier configuration

--- frontendd/src/components/Header.js
+++ frontendd/src/components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
-const navbarStyle={
-  backgroundColor:"red"
+const navbarStyle = {
+  backgroundColor: 'red',
 };
 
-const Header =() => {
-    return(
-        <Navbar style={navbarStyle} data-bs-theme="light">
-          <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
-      </Navbar>
-    )
+const Header = () => {
+  return (
+    <Navbar style={navbarStyle} data-bs-theme="light">
+      <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+    </Navbar>
+  );
 };
 
 export default Header;
